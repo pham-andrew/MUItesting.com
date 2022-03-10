492f520d85ff3da209f1fc1fc526977b435f74ce
Andrew Pham <anp6338@g.harvard.edu>
2022-03-09 21:37:49 -0700

    drawer styling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import * as React from 'react'
 import { useState } from 'react'
 import Helmet from 'react-helmet'
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom'
-import "@fontsource/plus-jakarta-sans";
 
+import "@fontsource/plus-jakarta-sans";
+import "@fontsource/ibm-plex-sans";
+
+import { ThemeProvider, createTheme } from '@mui/material'
 import Box from '@mui/material/Box'
 import Drawer from '@mui/material/Drawer'
 import AppBar from '@mui/material/AppBar'
@@ -16,7 +19,10 @@ import ListItemText from '@mui/material/ListItemText'
 import ListSubheader from '@mui/material/ListSubheader'
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu';
-import { createTheme, Tooltip } from '@mui/material'
+import Collapse from '@mui/material/Collapse';
+import { ListItemButton } from '@mui/material';
+
+import { ExpandMore, ExpandLess } from '@mui/icons-material';
 
 import Home from './Home'
 
@@ -47,214 +53,231 @@ const drawerWidth = 240;
 
 export default function PermanentDrawerLeft() {
   let navigate = useNavigate()
-  
-  const [open, setOpen] = useState(false)
+
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false)
+  const [isInputsOpen, setIsInputsOpen] = useState(false);
+  const [isDataDisplayOpen, setIsDataDisplayOpen] = useState(false);
+  const [isFeedbackOpen, setIsFeedbackOpen] = useState(false);
 
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Helmet >
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1523342493703569" crossorigin="anonymous"></script>
-      </Helmet>
-      <AppBar style={{ background: '#071a2d' }}>
-        <Toolbar>
-          <IconButton
-              size="large"
-              edge="start"
-              sx={{ mr: 2 }}
-              color="inherit"
-              onClick={()=>setOpen(true)}
-            >
-              <MenuIcon />
-          </IconButton>
-          <Typography onClick={()=>navigate('')} variant="h6" theme={createTheme({typography: {fontFamily:["Plus Jakarta Sans"]}})}>
-            MUI Testing
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      
-      <Drawer
-        sx={{
-          width: drawerWidth,
-          flexShrink: 0,
-          '& .MuiDrawer-paper': {
+    <ThemeProvider theme={createTheme({typography:{fontFamily:["IBM Plex Sans"]}})} >
+      <Box sx={{ display: 'flex' }}>
+        <Helmet >
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1523342493703569" crossorigin="anonymous"></script>
+        </Helmet>
+        <AppBar style={{ background: '#071a2d' }}>
+          <Toolbar>
+            <IconButton
+                size="large"
+                edge="start"
+                sx={{ mr: 2 }}
+                color="inherit"
+                onClick={()=>setIsDrawerOpen(true)}
+              >
+                <MenuIcon />
+            </IconButton>
+            <Typography onClick={()=>navigate('')} variant="h6" theme={createTheme({typography:{fontFamily:["Plus Jakarta Sans"]}})}>
+              MUI Testing
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        
+        <Drawer
+          sx={{
             width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        open={open}
-        onClose={()=>setOpen(false)}
-        anchor="left"
-      >
-        <Toolbar />
-        <Divider />
-        <List>
+            flexShrink: 0,
+            '& .MuiDrawer-paper': {
+              width: drawerWidth,
+              boxSizing: 'border-box',
+            },
+          }}
+          PaperProps={{
+            sx: {
+              backgroundColor: "#0A1929",
+              color: "#B2BAC2"
+            }
+          }}
+          open={isDrawerOpen}
+          onClose={()=>setIsDrawerOpen(false)}
+          anchor="left"
+        >
+          <Toolbar />
+          <Divider />
+          <List>
 
-          <ListItem button onClick={() => navigate('')}>
-            <ListItemText primary={"Home"}/>
-          </ListItem>
+            <ListItem button onClick={() => navigate('')}>
+              <ListItemText primary={"Home"}/>
+            </ListItem>
 
-          <ListSubheader>
-            Inputs
-          </ListSubheader>
-          <ListItem button onClick={() => navigate('autocomplete')}>
-            <ListItemText primary={"Autocomplete"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('button')}>
-            <ListItemText primary={"Button"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('buttongroup')}>
-            <ListItemText primary={"Button Group"} />
-          </ListItem>
-          <ListItem button onClick={() => navigate('checkbox')}>
-            <ListItemText primary={"Checkbox"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('fab')}>
-            <ListItemText primary={"Floating Action Button"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('radio')}>
-            <ListItemText primary={"Radio"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('rating')}>
-            <ListItemText primary={"Rating"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('select')}>
-            <ListItemText primary={"Select"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('slider')}>
-            <ListItemText primary={"Slider"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('switch')}>
-            <ListItemText primary={"Switch"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('textfield')}>
-            <ListItemText primary={"TextField"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('toggle')}>
-            <ListItemText primary={"ToggleButton"}/>
-          </ListItem>
+            <ListItemButton onClick={() => setIsInputsOpen(!isInputsOpen)}>
+              <ListItemText primary="Inputs" sx={{color: "white"}}/>
+              {isInputsOpen ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={isInputsOpen}>
+              <ListItem button onClick={() => navigate('autocomplete')}>
+                <ListItemText primary={"Autocomplete"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('button')}>
+                <ListItemText primary={"Button"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('buttongroup')}>
+                <ListItemText primary={"Button Group"} />
+              </ListItem>
+              <ListItem button onClick={() => navigate('checkbox')}>
+                <ListItemText primary={"Checkbox"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('fab')}>
+                <ListItemText primary={"Floating Action Button"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('radio')}>
+                <ListItemText primary={"Radio"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('rating')}>
+                <ListItemText primary={"Rating"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('select')}>
+                <ListItemText primary={"Select"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('slider')}>
+                <ListItemText primary={"Slider"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('switch')}>
+                <ListItemText primary={"Switch"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('textfield')}>
+                <ListItemText primary={"TextField"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('toggle')}>
+                <ListItemText primary={"ToggleButton"}/>
+              </ListItem>
+            </Collapse>
 
-          <ListSubheader>
-            Data Display
-          </ListSubheader>
-          <ListItem button onClick={() => navigate('avatar')}>
-            <ListItemText primary={"Avatar"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('badge')}>
-            <ListItemText primary={"Badge"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('chip')}>
-            <ListItemText primary={"Chip"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('divider')}>
-            <ListItemText primary={"Divider"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('icon')}>
-            <ListItemText primary={"Icon"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('sortable')}>
-            <ListItemText primary={"SorTable"}/>
-          </ListItem>
-          <ListItem button onClick={() => navigate('tooltip')}>
-            <ListItemText primary={"Tooltip"}/>
-          </ListItem>
+            <ListItemButton onClick={() => setIsDataDisplayOpen(!isDataDisplayOpen)}>
+              <ListItemText primary="Data Display" sx={{color: "white"}}/>
+              {isDataDisplayOpen ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={isDataDisplayOpen}>
+              <ListItem button onClick={() => navigate('avatar')}>
+                <ListItemText primary={"Avatar"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('badge')}>
+                <ListItemText primary={"Badge"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('chip')}>
+                <ListItemText primary={"Chip"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('divider')}>
+                <ListItemText primary={"Divider"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('icon')}>
+                <ListItemText primary={"Icon"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('sortable')}>
+                <ListItemText primary={"SorTable"}/>
+              </ListItem>
+              <ListItem button onClick={() => navigate('tooltip')}>
+                <ListItemText primary={"Tooltip"}/>
+              </ListItem>
+            </Collapse>
 
+            <ListItemButton onClick={() => setIsFeedbackOpen(!isFeedbackOpen)}>
+              <ListItemText primary="Feedback" sx={{color: "white"}}/>
+              {isFeedbackOpen ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={isFeedbackOpen}>
+              <ListItem button onClick={() => navigate('snackbar')}>
+                <ListItemText primary={"Snackbar"}/>
+              </ListItem>
+            </Collapse>
 
-          <ListSubheader>
-            Feedback
-          </ListSubheader>
-          <ListItem button onClick={() => navigate('snackbar')}>
-            <ListItemText primary={"Snackbar"}/>
-          </ListItem>
+            {/* Other Helpful Test Examples
+            <ListItem button disabled>
+              <ListItemText primary={"React Router"}/>
+            </ListItem>
+            <ListItem button disabled>
+              <ListItemText primary={"API Mocking"}/>
+            </ListItem>
+            <ListItem button disabled>
+              <ListItemText primary={"useContext"}/>
+            </ListItem>
+            <ListItem button disabled>
+              <ListItemText primary={"Query Examples, getBy..."}/>
+            </ListItem> */}
+            
+          </List>
+          <Divider />
+        </Drawer>
 
-          <ListSubheader>
-            Other Helpful Test Examples
-          </ListSubheader>
-          <ListItem button disabled>
-            <ListItemText primary={"React Router"}/>
-          </ListItem>
-          <ListItem button disabled>
-            <ListItemText primary={"API Mocking"}/>
-          </ListItem>
-          <ListItem button disabled>
-            <ListItemText primary={"useContext"}/>
-          </ListItem>
-          <ListItem button disabled>
-            <ListItemText primary={"Query Examples, getBy..."}/>
-          </ListItem>
-
-        </List>
-        <Divider />
-      </Drawer>
-
-      <Box
-        component="main"
-        sx={{ flexGrow: 1, bgcolor: 'background.default'}}
-      >
-        <Toolbar />
-        <Routes>
-            <Route path="autocomplete" element={<Box sx={{padding: 5}}><AutocompletePage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="button" element={<Box sx={{padding: 5}}><ButtonPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="buttongroup" element={<Box sx={{padding: 5}}><ButtonGroupPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="checkbox" element={<Box sx={{padding: 5}}><CheckboxPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="fab" element={<Box sx={{padding: 5}}><FloatingActionButtonPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="radio" element={<Box sx={{padding: 5}}><RadioPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="rating" element={<Box sx={{padding: 5}}><RatingPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="select" element={<Box sx={{padding: 5}}><SelectPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="slider" element={<Box sx={{padding: 5}}><SliderPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="snackbar" element={<Box sx={{padding: 5}}><SnackbarPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="sortable" element={<Box sx={{padding: 5}}><SorTablePage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="switch" element={<Box sx={{padding: 5}}><SwitchPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="textfield" element={<Box sx={{padding: 5}}><TextFieldPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="toggle" element={<Box sx={{padding: 5}}><ToggleButtonPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="avatar" element={<Box sx={{padding: 5}}><AvatarPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="badge" element={<Box sx={{padding: 5}}><BadgePage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="chip" element={<Box sx={{padding: 5}}><ChipPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="divider" element={<Box sx={{padding: 5}}><DividerPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="icon" element={<Box sx={{padding: 5}}><IconPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="tooltip" element={<Box sx={{padding: 5}}><TooltipPage /></Box>}/>
-        </Routes>
-        <Routes>
-            <Route path="" element={<Home setOpen={setOpen}/>}/>
-        </Routes>
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, bgcolor: 'background.default'}}
+        >
+          <Toolbar />
+          <Routes>
+              <Route path="autocomplete" element={<Box sx={{padding: 5}}><AutocompletePage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="button" element={<Box sx={{padding: 5}}><ButtonPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="buttongroup" element={<Box sx={{padding: 5}}><ButtonGroupPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="checkbox" element={<Box sx={{padding: 5}}><CheckboxPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="fab" element={<Box sx={{padding: 5}}><FloatingActionButtonPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="radio" element={<Box sx={{padding: 5}}><RadioPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="rating" element={<Box sx={{padding: 5}}><RatingPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="select" element={<Box sx={{padding: 5}}><SelectPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="slider" element={<Box sx={{padding: 5}}><SliderPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="snackbar" element={<Box sx={{padding: 5}}><SnackbarPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="sortable" element={<Box sx={{padding: 5}}><SorTablePage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="switch" element={<Box sx={{padding: 5}}><SwitchPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="textfield" element={<Box sx={{padding: 5}}><TextFieldPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="toggle" element={<Box sx={{padding: 5}}><ToggleButtonPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="avatar" element={<Box sx={{padding: 5}}><AvatarPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="badge" element={<Box sx={{padding: 5}}><BadgePage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="chip" element={<Box sx={{padding: 5}}><ChipPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="divider" element={<Box sx={{padding: 5}}><DividerPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="icon" element={<Box sx={{padding: 5}}><IconPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="tooltip" element={<Box sx={{padding: 5}}><TooltipPage /></Box>}/>
+          </Routes>
+          <Routes>
+              <Route path="" element={<Home setOpen={setIsDrawerOpen}/>}/>
+          </Routes>
+        </Box>
       </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
--- src/Home.js
+++ src/Home.js
@@ -17,7 +17,7 @@ export default function Home(props) {
                     >
                         The MaterialUI testing reference you needed</Typography>
                     <Typography 
-                        color="gray" 
+                        color="#B2BAC2" 
                         sx={{margin: 2, maxWidth: "650px"}}
                     >
                         This website provides a quick reference for testing Material UI components with React Testing Library. Simply click on the button below to see the components.
@@ -40,7 +40,9 @@ export default function Home(props) {
                         </Typography>
                     </Grid>
                     <Grid xs={12} sx={{margin: 2}}>
-                    <Typography color="gray">React recommends using Jest as the test runner and React Testing Library.</Typography>
+                    <Typography color="#B2BAC2">
+                        React recommends using Jest as the test runner and React Testing Library.
+                    </Typography>
                     </Grid>
                     <Grid xs={12} sx={{margin: 2}}>
                         <Typography 
@@ -53,7 +55,7 @@ export default function Home(props) {
                         </Typography>
                     </Grid>
                     <Grid xs={12} sx={{margin: 2}}>
-                    <Typography color="gray" sx={{maxWidth: "800px"}}>
+                    <Typography color="#B2BAC2" sx={{maxWidth: "800px"}}>
                         You may notice that there are a lot of aria-labels in our test examples. Why is that? To help find the element.
                         ARIA is a W3C specification that stands for “Accessible Rich Internet Applications”.
                         The aria-label provides the element with an accessible name, usually when there's no name already or when the content isn't visible.
@@ -72,7 +74,7 @@ export default function Home(props) {
                         </Typography>
                     </Grid>
                     <Grid xs={12} sx={{margin: 2}}>
-                    <Typography color="gray" sx={{maxWidth: "800px"}}>
+                    <Typography color="#B2BAC2" sx={{maxWidth: "800px"}}>
                         MaterialUI components abstract the actual HTML used so labels sometimes end up on a parent tag. To test, we often specifically want to act on the input. 
                         MaterialUI allows us to add properties to specifically the input of a MUI component by using the inputProps property. There we can label them to be found by our queries.
                     </Typography>
@@ -88,7 +90,7 @@ export default function Home(props) {
                         </Typography>
                     </Grid>
                     <Grid xs={12} sx={{margin: 2}}>
-                    <Typography color="gray" sx={{maxWidth: "800px"}}>
+                    <Typography color="#B2BAC2" sx={{maxWidth: "800px"}}>
                         This site was made by Andrew Pham hated testing and test driven development.
                     </Typography>
                     </Grid>
